4bcdcfa8803902f954ce3266a1eea0229e55d95a
Gregory Eremeev <gregory.eremeev@gmail.com>
2019-07-02 13:46:36 +0300

    Clarify the licence

--- setup.py
+++ setup.py
@@ -9,13 +9,18 @@ setup(
     author='Greg Eremeev',
     author_email='gregory.eremeev@gmail.com',
     version='0.3.0',
-    license='BSD',
+    license='BSD-3-Clause',
     url='https://github.com/GregEremeev/rosreestr-api',
     install_requires=requirements,
     description='Toolset to work with rosreestr.ru/api',
     packages=find_packages(),
     extras_require={'dev': ['ipdb==0.12', 'pytest==4.6.3', 'httpretty==0.9.6']},
-    classifiers=['Programming Language :: Python :: 3.7'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
     zip_safe=False,
     include_package_data=True
 )
