c2070297a466e8cc10fcc0b0e82098b60776b1b3
Gregory Eremeev <gregory.eremeev@gmail.com>
2019-07-02 13:34:45 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='rosreestr-api',
     author='Greg Eremeev',
     author_email='gregory.eremeev@gmail.com',
-    version='0.2.1',
+    version='0.3.0',
     license='BSD',
     url='https://github.com/GregEremeev/rosreestr-api',
     install_requires=requirements,
